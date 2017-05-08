f48d0fca2948ecbb3d7806b7a09250a1f4385a96
ibm_admin <ibm_admin@ADMINIB-U0RGV7D.in.ibm.com>
2017-05-08 15:33:17 +0530

    Signed-off-by: ibm_admin <ibm_admin@ADMINIB-U0RGV7D.in.ibm.com>

---
+++ src/test/java/TestCases/Flipkart/com/ProductSearch.java
@@ -0,0 +1,12 @@
+package TestCases.Flipkart.com;
+
+import org.testng.annotations.Test;
+
+public class ProductSearch 
+{
+	@Test
+	public void ProductInfo()
+	{
+		System.out.println("Welcome to Maven World");
+	}
+}
